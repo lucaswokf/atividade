b94b984092585f9a70fc0cc68021030d22e3d764
lucaswokf <lucasinfu@gmail.com>
2025-04-29 20:14:33 -0300

    Add files via upload

---
+++ src/components/Cartao.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import './Cartao.css';
+
+function Cartao(){
+    return(
+        <>
+            <header> <h1> Spyware: Como podemos evitar </h1></header>
+            <h2>O que é um  Spyware ?</h2>
+    <p>Spyware é um tipo de software malicioso (malware) que espiona o usuário sem o seu consentimento.Ele é instalado no dispositivo de forma oculta e coleta informações pessoais, como: Histórico de navegação
+
+Dados de login e senhas
+
+Informações bancárias
+
+Localização
+
+Mensagens e arquivos
+
+</p>
+
+    <h2> Seu Funciona?</h2>
+    <p>Spyware funciona como um espião digital que se instala silenciosamente no seu dispositivo, muitas vezes sem que você perceba. Ele pode vir escondido em arquivos baixados, anexos de e-mail ou até em sites maliciosos. Após ser instalado, ele começa a rodar em segundo plano, sem mostrar sinais visíveis, o que dificulta sua detecção por usuários comuns.
+
+</p>
+
+    <h2>Como se Proteger contra Spyware?</h2>
+    <ul>
+      <li><strong>🔒 1. Use um antivírus/antimalware confiável</strong>: Instale um programa de segurança e mantenha-o sempre atualizado.
+
+                                                                        Faça varreduras regulares no sistema.</li>
+      <li><strong>🔄 2. Mantenha o sistema e os apps atualizados</strong>: Atualizações corrigem falhas de segurança exploradas por spyware.
+
+                                                                            Isso vale para Windows, Android, iOS e programas em geral.</li>
+
+      <li><strong>📩 3. Evite clicar em links e anexos suspeitos</strong>: Não abra e-mails ou mensagens de remetentes desconhecidos.
+
+                                                                          Cuidado com promoções ou alertas falsos.</li>
+    </ul>
+    <h3>Dica Extra:</h3>
+      <p>Use navegador seguro e extensões de proteção.
+
+Fique atento a sinais estranhos no dispositivo.
+
+Ative autenticação em duas etapas (2FA) para proteger suas contas. </p>
+    
+    <ul>
+     
+    </ul>
+        </>
+    )
+
+};
+export default Cartao;  
